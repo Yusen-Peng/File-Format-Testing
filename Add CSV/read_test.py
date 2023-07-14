4ab21b37848dc0f277c6f5183817ac97e2d9eb03
Yusen Peng <125777793+Yusen-Peng@users.noreply.github.com>
2023-07-14 19:29:13 -0400

    Add files via upload

---
+++ Add CSV/read_test.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import time
+import h5py
+import zarr
+import csv
+import pandas as pd
+from netCDF4 import Dataset
+import numpy as np
+
+def read(file_format, filename, num_datasets, dimensions):
+
+    dataset_read_time = 0.0
+
+    # Open files according to the format
+    if file_format == 'HDF5':
+        file = h5py.File(f'files_2/{filename}_copy.hdf5', 'r')
+    elif file_format == 'netCDF4':
+        file = Dataset(f'files_2/{filename}_copy.netc', 'r')
+    elif file_format == 'Zarr':
+        file = zarr.open(f'files_2/{filename}_copy.zarr', 'r')
+
+
+    # Open a dataset
+    for i in range(0, num_datasets):
+        if file_format == 'HDF5':
+            dataset = file[f'Dataset_{i}']
+        elif file_format == 'netCDF4':
+            dataset = file.variables[f'Dataset_{i}']
+        elif file_format == 'Zarr':
+            dataset = file.get(f'Dataset_{i}')
+        
+        #load CSV files
+        else:
+            t1 = time.perf_counter()
+            if len(dimensions) == 1: 
+                #load the CSV file
+                dataset = np.loadtxt(f'CSV_data/CSV_data_{i}.csv', delimiter=',')
+            else:
+                #load the CSV file
+                dataset = np.loadtxt(f'CSV_data/CSV_data_{i}.csv', delimiter=',')
+                #reshape into a matrix
+                dataset = dataset.reshape(dimensions[0], dimensions[1])
+        
+
+        # record read-time 
+        if len(dimensions) == 1:                     
+            t1 = time.perf_counter()
+            print(dataset[:dimensions[0]])
+        elif len(dimensions) == 2:
+            t1 = time.perf_counter()
+            print(dataset[:dimensions[0], :dimensions[1]])
+        else:
+            t1 = time.perf_counter()
+            print(dataset[:dimensions[0], :dimensions[1], :dimensions[2]])
+        t2 = time.perf_counter()
+
+        # calculate
+        dataset_read_time += (t2- t1)
+
+
+    # Close the file (if applicable) and delete it to save space
+    if not file_format == 'Zarr' and not file_format == 'CSV':
+        file.close() 
+                                    # close if HDF5 or netCDF4
+        if file_format == 'HDF5':
+            os.remove(f'files_2/{filename}_copy.hdf5')
+        elif file_format == 'netCDF4':
+            os.remove(f'files_2/{filename}_copy.netc')
+
+    elif file_format == 'Zarr':
+        shutil.rmtree(f'files_2/{filename}_copy.zarr')
+                                    # if Zarr: shutil.rmtree (delete an entire directory tree)
+
+    # Times are in milliseconds
+    return 1000 * dataset_read_time / num_datasets
---
+++ Add CSV/runner.py
@@ -0,0 +1,49 @@
+import csv
+import glob
+import yaml 
+import plot_test, read_test, write_test
+
+
+def run_benchmark(config_file, file_formats, num_trials):
+
+    with open(f'{config_file}', 'r') as file:
+        config = yaml.safe_load(file)
+        filename = config.get('FILE_NAME')
+        num_datasets = config.get('NUMBER_DATASETS')
+        dimensions = config.get('NUMBER_ELEMENTS')
+                                                            # get configuration data from the configuration file
+
+    for file_format in file_formats:
+        
+        csvfile = open(f'csv_file/{file_format}_{num_datasets}_{dimensions}.csv', 'w')
+        writer = csv.writer(csvfile)
+        writer.writerow([file_format, 'Dataset Write Time','Dataset Read Time'])
+        for i in range(num_trials):
+            results_write = write_test.write(file_format, filename, num_datasets, dimensions)
+            results_read = read_test.read(file_format, filename, num_datasets, dimensions)
+            writer.writerow([f'Trial {i + 1}', results_write, results_read])
+        csvfile.close()
+                                                            #Create a CSV file to store the data for a given file format
+
+    plot_test.plot(file_formats, num_datasets, dimensions)
+                                                            #plot final results 
+
+
+def main(file_formats, num_trials):
+
+    config_files = glob.glob('config/*.yaml')
+    
+    for config_file in config_files:
+        run_benchmark(config_file,file_formats,num_trials)
+                                                            #run benchmarks for every configuration file
+
+
+
+if __name__ == '__main__':
+    file_formats = ['CSV','HDF5','netCDF4','Zarr']
+                                                            # 3 file format to explore
+
+    num_trials = 5  
+                                                            # Must be greater than 1
+
+    main(file_formats, num_trials)
